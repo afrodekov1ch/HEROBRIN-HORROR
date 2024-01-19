b890d5c3c07c707c9710623218413cf73b70e644
Akaha LoKs <132211327+akaha-loks@users.noreply.github.com>
2024-01-19 16:08:10 +0600

    Scene Random

--- HORROR HEROBRIN/Assets/Scripts/ChooseLvL.cs
+++ HORROR HEROBRIN/Assets/Scripts/ChooseLvL.cs
@@ -1,79 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 public class ChooseLvL : MonoBehaviour
 {
-    private int chooseLvl;
-    private int[] saveChoose;
+    public int[] myArray;
 
-    private void Update()
+    void Awake()
     {
-        saveChoose[0] = PlayerPrefs.GetInt("lvl1");
-        saveChoose[1] = PlayerPrefs.GetInt("lvl2");
-        saveChoose[2] = PlayerPrefs.GetInt("lvl3");
-        saveChoose[3] = PlayerPrefs.GetInt("lvl4");
-        saveChoose[4] = PlayerPrefs.GetInt("lvl5");
-        saveChoose[5] = PlayerPrefs.GetInt("lvl6");
-        saveChoose[6] = PlayerPrefs.GetInt("lvl7");
-        saveChoose[7] = PlayerPrefs.GetInt("lvl8");
-        saveChoose[8] = PlayerPrefs.GetInt("lvl9");
-        saveChoose[9] = PlayerPrefs.GetInt("lvl10");
-    }
-    public void LoadScene(int lvl)
-    {
-        chooseLvl = Random.Range(3, 17);
-        if (lvl == 1)
-        {
-            PlayerPrefs.SetInt("lvl1", chooseLvl);   
-            SceneManager.LoadScene(chooseLvl);
-        }
-        if (lvl == 2 && saveChoose[0] != chooseLvl)
-        {
-            PlayerPrefs.SetInt("lvl2", chooseLvl);
-            SceneManager.LoadScene(chooseLvl);
-        }
-        if (lvl == 3 && saveChoose[0] != chooseLvl && saveChoose[1] != chooseLvl)
-        {
-            PlayerPrefs.SetInt("lvl3", chooseLvl);
-            SceneManager.LoadScene(chooseLvl);
-        }
-        if (lvl == 4 && saveChoose[0] != chooseLvl && saveChoose[1] != chooseLvl && saveChoose[2] != chooseLvl && saveChoose[3] != chooseLvl)
-        {
-            PlayerPrefs.SetInt("lvl4", chooseLvl);
-            SceneManager.LoadScene(chooseLvl);
-        }
-        if (lvl == 5 && saveChoose[0] != chooseLvl && saveChoose[1] != chooseLvl && saveChoose[2] != chooseLvl && saveChoose[3] != chooseLvl && saveChoose[4] != chooseLvl)
-        {
-            PlayerPrefs.SetInt("lvl5", chooseLvl);
-            SceneManager.LoadScene(chooseLvl);
-        }
-        if (lvl == 6 && saveChoose[0] != chooseLvl && saveChoose[1] != chooseLvl && saveChoose[2] != chooseLvl && saveChoose[3] != chooseLvl && saveChoose[4] != chooseLvl && saveChoose[5] != chooseLvl)
-        {
-            PlayerPrefs.SetInt("lvl6", chooseLvl);
-            SceneManager.LoadScene(chooseLvl);
-        }
-        if (lvl == 7 && saveChoose[0] != chooseLvl && saveChoose[1] != chooseLvl && saveChoose[2] != chooseLvl && saveChoose[3] != chooseLvl && saveChoose[4] != chooseLvl && saveChoose[5] != chooseLvl && saveChoose[6] != chooseLvl)
-        {
-            PlayerPrefs.SetInt("lvl7", chooseLvl);
-            SceneManager.LoadScene(chooseLvl);
-        }
-        if (lvl == 8 && saveChoose[0] != chooseLvl && saveChoose[1] != chooseLvl && saveChoose[2] != chooseLvl && saveChoose[3] != chooseLvl && saveChoose[4] != chooseLvl && saveChoose[5] != chooseLvl && saveChoose[6] != chooseLvl && saveChoose[7] != chooseLvl)
-        {
-            PlayerPrefs.SetInt("lvl8", chooseLvl);
-            SceneManager.LoadScene(chooseLvl);
-        }
-        if (lvl == 9 && saveChoose[0] != chooseLvl && saveChoose[1] != chooseLvl && saveChoose[2] != chooseLvl && saveChoose[3] != chooseLvl && saveChoose[4] != chooseLvl && saveChoose[5] != chooseLvl && saveChoose[6] != chooseLvl && saveChoose[7] != chooseLvl && saveChoose[8] != chooseLvl)
-        {
-            PlayerPrefs.SetInt("lvl9", chooseLvl);
-            SceneManager.LoadScene(chooseLvl);
-        }
-        if (lvl == 10 && saveChoose[0] != chooseLvl && saveChoose[1] != chooseLvl && saveChoose[2] != chooseLvl && saveChoose[3] != chooseLvl && saveChoose[4] != chooseLvl && saveChoose[5] != chooseLvl && saveChoose[6] != chooseLvl && saveChoose[7] != chooseLvl && saveChoose[8] != chooseLvl && saveChoose[9] != chooseLvl)
-        {
-            PlayerPrefs.SetInt("lvl10", chooseLvl);
-            SceneManager.LoadScene(chooseLvl);
-        }
+        myArray = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
+        for (int i = myArray.Length - 1; i > 0; i--)
+        {
+            int randomIndex = Random.Range(0, i + 1);
+            int temp = myArray[i];
+            myArray[i] = myArray[randomIndex];
+            myArray[randomIndex] = temp;
+        }
+        foreach (var num in myArray)
+        {
+            Debug.Log(num);
+        }
+        PlayerPrefs.SetInt("lvl0", myArray[0]);
+        PlayerPrefs.SetInt("lvl1", myArray[1]);
+        PlayerPrefs.SetInt("lvl2", myArray[2]);
+        PlayerPrefs.SetInt("lvl3", myArray[3]);
+        PlayerPrefs.SetInt("lvl4", myArray[4]);
+        PlayerPrefs.SetInt("lvl5", myArray[5]);
+        PlayerPrefs.SetInt("lvl6", myArray[6]);
+        PlayerPrefs.SetInt("lvl7", myArray[7]);
+        PlayerPrefs.SetInt("lvl8", myArray[8]);
+        PlayerPrefs.SetInt("lvl9", myArray[9]);
     }
 }
 
