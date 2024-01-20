ff1383012bdef71744d19ea27714035d9280b11f
Akaha LoKs <132211327+akaha-loks@users.noreply.github.com>
2024-01-20 15:22:54 +0600

    Сделал систему хп(во всех сценах)

---
+++ HORROR HEROBRIN/Assets/Scripts/StartGame.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+    private int health;
+    private void Awake()
+    {
+        health = PlayerPrefs.GetInt("health");
+        if (health < 3 || health >= 4)
+        {
+            Debug.LogWarning("’œ" + health);
+            health = 3;
+            PlayerPrefs.SetInt("health", health);
+        }
+    }
+}
